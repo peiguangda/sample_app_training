0d67af402e9d7114b78612a76b317b8822c8f536
peiguangda <peiguangda@gmail.com>
2018-07-14 02:06:33 +0700

    chap 13_part1

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: :edit
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
---
+++ db/micropost_seed.rb
@@ -0,0 +1,5 @@
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
