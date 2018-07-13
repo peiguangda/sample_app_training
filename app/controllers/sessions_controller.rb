34d16d88a9d2df749361c85754e8c3d31101b527
peiguangda <peiguangda@gmail.com>
2018-07-13 11:25:52 +0700

    chap 10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if user&.authenticate params[:session][:password]
       log_in user
       remember_user user
-      redirect_to user
+      redirect_back_or user
     else
       flash[:danger] = t "login_invalid_msg"
       render :new
